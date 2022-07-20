54bb0b8e25750d9a65363a591315fe63075049f8
shamil-v <shamil@hamon.in>
2022-07-20 10:33:08 +0530

    navigation demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import About from './components/About';
+import Profile from './components/Profile';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 function App() {
+  const [state, setState] = useState('');
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => navigate('/about')}>About</button>
+      <button onClick={() => navigate('/profile')}>Profile</button>
+
+      <Routes>
+        <Route element={<About />} path='/about' />
+        <Route element={<Profile />} path='/profile' />
+      </Routes>
     </div>
   );
 }
